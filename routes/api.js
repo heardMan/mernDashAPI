5e4a2399080a71b2c64e7293caa94b261b457a7f
heardman <markyheard@gmail.com>
2020-08-24 19:51:25 -0700

    making sure everythin is up to date

--- routes/api.js
+++ routes/api.js
@@ -18,6 +18,16 @@ router.get("/orders", function (req, res) {
       console.log(err);
     }
     console.log(docs);
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
     res.json(docs);
   });
 
